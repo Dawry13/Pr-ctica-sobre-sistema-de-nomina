0fb070a2533834ad1a6fef2a3e49d1c1c64cabf0
Alexis Dawry Encarnacion Encarnacion <20242437@itla.edu.do>
2026-09-15 19:42:39 -0400

    Mover Clase

---
+++ Modelos/Empleados/Empleado.cs
@@ -0,0 +1,28 @@
+﻿namespace Práctica_sobre_sistema_de_nomina.Modelos.Empleados
+{
+    public abstract class Empleado
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string nss { get; set; }
+
+        public Empleado(
+            string Nombre,
+            string apellido,
+            string nss)
+        {
+            this.Nombre = Nombre;
+            Apellido = apellido;
+            this.nss = nss;
+        }
+
+        public abstract decimal CalcularPago();
+
+        public override string ToString()
+        {
+            return $"Nombre: {Nombre}\n" +
+                   $"Apellido: {Apellido}\n" +
+                   $"NSS: {nss}";
+        }
+    }
+}
